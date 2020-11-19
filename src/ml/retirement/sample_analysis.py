d4384743eaace2e7cf90ea1abf885ef164459c23
Omkar Ranadive <omkar.r1997@gmail.com>
2020-11-18 18:31:05 -0600

    Updates to selection using reliability scores

--- src/ml/retirement/sample_analysis.py
+++ src/ml/retirement/sample_analysis.py
@@ -4,7 +4,7 @@ Functions to analyze the zooniverse data
 
 import pandas as pd
 import json
-from constants import DATA_PATH, META_PATH, label_dict
+from constants import DATA_PATH, META_PATH, label_dict, index_to_label
 from collections import defaultdict, Counter
 from src.ml.retirement.r_utils import map_users_to_index, map_index_to_users
 from utils import load_file
@@ -12,7 +12,7 @@ import os
 import numpy as np
 
 
-def extract_sub_ids(file, naive=False):
+def extract_sub_ids(file, naive=False, label_of_interest=0):
     """
     Extracts sub_ids from dataset_x.txt files of the following format:
     SubjectID,Network,Station,Latitude,Longitude,PercentageEarthquakes,PercentageTremor,
@@ -22,44 +22,25 @@ def extract_sub_ids(file, naive=False):
         file (path): Path to file containing the dataset info
         naive (bool): If naive = True, apply naive selection based on threshold (no reliability
         score)
+        label_of_interest (int): Only used if naive flag is set to true
 
     Returns (list): List containing extracted subject ids
     """
 
     sub_ids = []
     threshold = 70.0
+    offset_dict = {0: 5, 1: 7, 2: 6, 3: 7}  # Map labels to indices according to headers
+    label_index = offset_dict[label_of_interest]
+
     with open(file, 'r') as f:
         for index, line in enumerate(f.readlines()):
             # Don't consider the headers
             if index > 2:
                 info = line.split(",")
                 if not naive:
-                    sub_ids.append(info[0])
-                elif naive and float(info[5]) > threshold:
-                    sub_ids.append(info[0])
-
-    return sub_ids
-
-
-def naive_selection(path):
-    """
-    This function selects subject ids without consideration of the reliability scores
-    i.e., all users are given equal weightage
-
-    Returns (list): List of selected ids
-
-    """
-    sub_ids = []
-    threshold = 70.0
-    with open(file, 'r') as f:
-        for index, line in enumerate(f.readlines()):
-            # Don't consider the headers
-            if index > 2:
-                info = line.split(",")
-                if float(info[5]) >= threshold:
-                    sub_ids.append(info[0])
-
-    print("Selected {} ids out of {}".format(len(sub_ids), index+1))
+                    sub_ids.append(int(info[0]))
+                elif naive and float(info[label_index]) > threshold:
+                    sub_ids.append(int(info[0]))
 
     return sub_ids
 
@@ -95,15 +76,15 @@ def selection_criteria_1(users, label_of_interest):
         return 0
 
 
-def select_sub_ids(sub_ids, classifications):
+def select_sub_ids(sub_ids, classifications, label_of_interest):
 
     sub_stats = gen_subids_stats(sub_ids, classifications)
     selected_ids = []
     for sub, users in sub_stats.items():
-        out = selection_criteria_1(users, label_of_interest=0)
+        out = selection_criteria_1(users, label_of_interest=label_of_interest)
 
         if out == 1:
-            selected_ids.append(sub)
+            selected_ids.append(int(sub))
 
     print("Selected {} ids out of {}".format(len(selected_ids), len(sub_stats)))
 
@@ -149,12 +130,12 @@ def gen_subids_stats(sub_ids, classifications):
         #
         # print(rel_scores[uin])
         f_score = rel_scores[uin][label_in][-1]
-        f_score = mean_scores[label_in] if np.isnan(f_score) else f_score
+        # f_score = mean_scores[label_in] if np.isnan(f_score) else f_score
+        f_score = 0.5 if np.isnan(f_score) else f_score
 
         sub_stats[sub_id].append((uin, label_in, f_score))
 
     return sub_stats
-
 
 if __name__ == '__main__':
 
@@ -163,14 +144,37 @@ if __name__ == '__main__':
     """
     folder = DATA_PATH / 'EQ_Vivian_Analysis'
     all_ids = []
+    label_of_interest = 2
     for file in os.listdir(folder):
-        print("Processing {}".format(file))
-        all_ids = extract_sub_ids(folder / file)
-        select_sub_ids(all_ids, classifications='earthquake-detective-classifications.csv')
-        # For comparison, also select them naively
-        naive_ids = extract_sub_ids(folder/file, naive=True)
-        print("Naive: Selected {} ids out of {}".format(len(naive_ids), len(all_ids)))
-        common_ids = set(all_ids).intersection(set(naive_ids))
-        print("{} ids were selected in common".format(len(common_ids)))
-        print("*"*20)
+        if not os.path.isdir(folder / file):
+            print("Processing {}".format(file))
+            all_ids = extract_sub_ids(folder / file)
+            selected_ids = select_sub_ids(all_ids,
+                           classifications='earthquake-detective-classifications.csv',
+                           label_of_interest=label_of_interest)
+            # For comparison, also select them naively
+            naive_ids = extract_sub_ids(folder/file, naive=True, label_of_interest=label_of_interest)
+            # print(selected_ids)
+            # print(naive_ids)
+            new_file_name = file[:-3] + '_result_' + index_to_label[label_of_interest] + '.txt'
+            with open(folder / 'Results_Criteria1' / new_file_name, 'w') as f:
+                f.write("Criteria1: Selected {} ids out of {}\n".format(len(selected_ids), len(all_ids)))
+                print("Naive: Selected {} ids out of {}".format(len(naive_ids), len(all_ids)))
+                f.write("Naive: Selected {} ids out of {}\n".format(len(naive_ids), len(all_ids)))
+                common_ids = set(selected_ids).intersection(set(naive_ids))
+                diff_ids = set(selected_ids).difference(set(naive_ids))
+                print("{} ids were selected in common".format(len(common_ids)))
+                f.write("{} ids were selected in common \n".format(len(common_ids)))
+                print("*"*20)
+
+                # Save different ids to file
+                f.write("New selected ids are as follows: \n")
+                for nid in diff_ids:
+                    f.write(str(nid) + '\n')
+
+                # Save common ids to file
+                f.write("The common ids are as follows: \n")
+                for cid in common_ids:
+                    f.write(str(cid) + '\n')
+
 
---
+++ src/scratch.py
@@ -0,0 +1,4 @@
+a = {"luffy", "Luffy", "muffy"}
+b  = {"Zoro", "luffy", "Nani"}
+
+print(set(a).difference(set(b)))
